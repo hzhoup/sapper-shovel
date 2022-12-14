ddafa8c5a614083c9500c5e172f30cd046643786
hzp <hzhouping@outlook.com>
2022-12-14 17:34:11 +0800

    feat: 添加 eslint-config 包

---
+++ packages/shovel-eslint-config/index.js
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+    browser: true,
+  },
+  extends: ['eslint-config-standard', 'plugin:@typescript-eslint/recommended'],
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    ecmaVersion: 'esnext',
+    sourceType: 'module',
+    extraFileExtensions: ['.vue'],
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {},
+}
