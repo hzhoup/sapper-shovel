7096fe5aebfbbcf0eae5d39df6e4207c3108387b
hzp <hzhouping@outlook.com>
2022-12-16 10:52:30 +0800

    fix: 修复包路径引用问题

--- packages/shovel-cli/lib/src/shared/constant.js
+++ packages/shovel-cli/lib/src/shared/constant.js
@@ -4,4 +4,4 @@ export const dirname = getDirname(import.meta.url);
 export const CWD = process.cwd();
 export const PRETTIER_EXTENSIONS = ['vue', 'ts', 'js', 'mjs', 'tsx', 'jsx', 'json', 'md'];
 export const ESLINT_EXTENSIONS = ['.vue', '.ts', '.js', '.mjs', '.tsx', '.jsx'];
-export const CLI_PACKAGE_JSON = resolve(dirname, '../../package.json');
+export const CLI_PACKAGE_JSON = resolve(dirname, '../../../package.json');
--- packages/shovel-cli/src/shared/constant.ts
+++ packages/shovel-cli/src/shared/constant.ts
@@ -7,4 +7,4 @@ export const CWD = process.cwd()
 
 export const PRETTIER_EXTENSIONS = ['vue', 'ts', 'js', 'mjs', 'tsx', 'jsx', 'json', 'md']
 export const ESLINT_EXTENSIONS = ['.vue', '.ts', '.js', '.mjs', '.tsx', '.jsx']
-export const CLI_PACKAGE_JSON = resolve(dirname, '../../package.json')
+export const CLI_PACKAGE_JSON = resolve(dirname, '../../../package.json')
